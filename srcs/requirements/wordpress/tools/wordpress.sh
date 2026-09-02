ea3a8758cb63d860b1455b2fe3f7e2940c7c7b36
Soufiane El Abboubi <140103287+Moraouid@users.noreply.github.com>
2026-09-03 00:23:16 +0100

    feat: initialize wordpress on startup

---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
+ADMIN_PASSWORD=$(cat "$ADMIN_PASSWORD_FILE")
+USER_PASSWORD=$(cat "$USER_PASSWORD_FILE")
+
+while ! mysqladmin ping -h"mariadb" --silent; do
+    sleep 1
+done
+
+cd /var/www/html
+
+if [ ! -f wp-config.php ]; then
+
+    wp core download --allow-root
+
+    wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --allow-root
+
+    wp core install --url="$DOMAIN_NAME" --title="Inception" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
+
+    wp user create "$USER" "$USER_EMAIL" --role=author --user_pass="$USER_PASSWORD" --allow-root
+
+fi
+
+exec php-fpm8.2 -F
