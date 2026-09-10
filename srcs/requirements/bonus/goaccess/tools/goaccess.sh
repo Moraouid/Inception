4fc95fb9bdf17f56f3c892f3981e8f700f37477f
Soufiane El Abboubi <selabboubi55@gmail.com>
2026-09-10 08:32:17 +0100

    feat(goaccess): serve auto-refreshing analytics dashboard

--- srcs/requirements/bonus/goaccess/tools/goaccess.sh
+++ srcs/requirements/bonus/goaccess/tools/goaccess.sh
@@ -8,8 +8,8 @@ cd /var/www/html
 
 while true; do
     goaccess /var/log/nginx/access.log -o /var/www/html/index.html --log-format=COMBINED
-    sed -i 's/<head>/<head><meta http-equiv="refresh" content="5">/' /var/www/html/index.html
-    sleep 5
+    sed -i 's/<head>/<head><meta http-equiv="refresh" content="10">/' /var/www/html/index.html
+    sleep 10
 done &
 
 exec python3 -m http.server 3001
